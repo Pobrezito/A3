61c22257dafcf8468905d1895709ec0c32543c02
Juan Java Master <142504073+Pobrezito@users.noreply.github.com>
2024-04-03 11:51:18 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+function criarUsuario(body) {
+    //logica para criação de usuarios no banco de dados
+    return { nome: body.name, id: 1 }
+}
+
+app.post('/users', (req, res) => {
+    //logica para cirar usuario
+    const usuario = criarUsuario(req.body);
+
+    res.json(usuario);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor Rodando`);
+});
+
+module.exports = { criarUsuario}
