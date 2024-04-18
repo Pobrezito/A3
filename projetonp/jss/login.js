23460ae7b3b77161d57bf7074861290a230dfc78
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-04-18 20:44:50 -0300

    commit fix att
    criação da pagina de feedback, padronização de pastas para fotos/htmls/css e js

--- projetonp/jss/login.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("loginButton").addEventListener("click", function() {
-    // Supondo que o login foi bem-sucedido, redirecione para a página desejada
-    window.location.href = "../login/pesquisa.html";
-});
