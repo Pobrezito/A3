839ae21871685f947f8a154da0d65b4b334137c1
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-04-23 09:08:39 -0300

    Create searchLanguages.test.js

---
+++ test/searchLanguages.test.js
@@ -0,0 +1,8 @@
+const searchLanguages = require('./searchLanguages');
+
+test('searchLanguages retorna os idiomas correspondentes à busca', () => {
+  const languages = ['JavaScript', 'Python', 'Java', 'C++', 'Ruby', 'Typescript'];
+  const searchTerm = 'Ja';
+  const expectedLanguages = ['JavaScript', 'Java'];
+  expect(searchLanguages(languages, searchTerm)).toEqual(expectedLanguages);
+});
