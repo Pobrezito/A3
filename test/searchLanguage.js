ebcdfe6107373564668530f66847604937f75726
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2024-04-23 09:07:50 -0300

    Create searchLanguage.js

---
+++ test/searchLanguage.js
@@ -0,0 +1,7 @@
+ function searchLanguages(languages, searchTerm) {
+  return languages.filter(language =>
+    language.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+}
+
+module.exports = searchLanguages;
