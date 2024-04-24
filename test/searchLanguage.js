7003fe2c1d969c5ca3407b74cb204472c746c5c9
GMastrillo <GMastrillo.kakashigabe232@gmail.com>
2024-04-23 23:31:14 -0300

    arquivo inutil

--- test/searchLanguage.js
+++
@@ -1,8 +0,0 @@
- function searchLanguages(languages, searchTerm) {
-  return languages.filter(language =>
-    language.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-}
-
-module.exports = searchLanguages;
-
