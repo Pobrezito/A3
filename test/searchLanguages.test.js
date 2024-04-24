358b4ef779489c732bed0d2f88d886cfe9557406
GMastrillo <GMastrillo.kakashigabe232@gmail.com>
2024-04-23 23:03:50 -0300

    Chore: correcao da funcionalidade searchlanguage.test

--- test/searchLanguages.test.js
+++ test/searchLanguages.test.js
@@ -1,9 +1,8 @@
-const searchLanguage = require('./searchLanguage');
+import searchLanguage from './searchLanguage'; // Se estiver usando import/export
 
 test('searchLanguage retorna os idiomas correspondentes à busca', () => {
   const languages = ['JavaScript', 'Python', 'Java', 'C++', 'Ruby', 'Typescript'];
   const searchTerm = 'Ja';
   const expectedLanguage = ['JavaScript', 'Java'];
-  expect(searchLanguage(language, searchTerm)).toEqual(expectedLanguage);
+  expect(searchLanguage(languages, searchTerm)).toEqual(expectedLanguage);
 });
-
